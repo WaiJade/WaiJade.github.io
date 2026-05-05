1cb8e184e3ed84b9ba36ec6b439eddeffa756f7d
WaiJade <waijade@outlook.com>
2026-05-05 12:44:26 +0800

    chore(friends): 新增友链

--- src/content/friends.ts
+++ src/content/friends.ts
@@ -11,5 +11,17 @@ export const friendsLinks: FriendLink[] = [
     url: "https://zaona.top/",
     avatar: "https://zaona.top/avatar.png",
     description: "Explore The Edge Of Imagination"
+  },
+  {
+    name: "hrsthrt74",
+    url: "https://hrsthrt74.github.io/",
+    avatar: "https://avatars.githubusercontent.com/u/49299205",
+    description: "hrsthrt74的博客"
+  },
+  {
+    name: "OrPudding",
+    url: "https://orpu.moe/",
+    avatar: "https://orpu.moe/upload/F337D8E876660646C6C3675320A2ABFD.webp",
+    description: "OrPudding的博客"
   }
 ];
